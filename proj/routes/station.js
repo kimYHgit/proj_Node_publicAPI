d6902ab3ceb6e6d4f3f293eb3303e66c77939ff7
jibinlim <130441571+jibinlim@users.noreply.github.com>
2023-05-26 16:21:25 +0900

    Update station.js

--- proj/routes/station.js
+++ proj/routes/station.js
@@ -7,37 +7,37 @@ const stationUtil = require('../lib/stationUtil');
 const { isLoggedIn } = require('../lib/middleware');
 
 // 등록
-// router.post('/', async (req, res) => {
-//   try {
-//     const params = {
-//       statnId: req.body.statnId,
-//       statnNm: req.body.statnNm,
-//       subwayNm: req.body.subwayNm,
-//       subwayId: req.body.subwayId,
-//       statnFid: req.body.statnFid,
-//       statnTid: req.body.statnTid,
-//       trnsitCo: req.body.trnsitCo,
-//     };
-//     logger.info(`(station.reg.params) ${JSON.stringify(params)}`);
+router.post('/', async (req, res) => {
+  try {
+    const params = {
+      statnId: req.body.statnId,
+      statnNm: req.body.statnNm,
+      subwayNm: req.body.subwayNm,
+      subwayId: req.body.subwayId,
+      statnFid: req.body.statnFid,
+      statnTid: req.body.statnTid,
+      trnsitCo: req.body.trnsitCo,
+    };
+    logger.info(`(station.reg.params) ${JSON.stringify(params)}`);
 
-//     // 입력값 null 체크
-//     if (!params.statnId || !params.statnNm || !params.subwayId || !params.trnsitCo) {
-//       const err = new Error('Not allowed null (statnId,stNm,arsId,statnId)');
-//       logger.error(err.toString());
+    // 입력값 null 체크
+    if (!params.statnId || !params.statnNm || !params.subwayId || !params.trnsitCo) {
+      const err = new Error('Not allowed null (statnId,stNm,arsId,statnId)');
+      logger.error(err.toString());
 
-//       res.status(500).json({ err: err.toString() });
-//     }
+      res.status(500).json({ err: err.toString() });
+    }
 
-//     // 비즈니스 로직 호출
-//     const result = await stationService.reg(params);
-//     logger.info(`(station.reg.result) ${JSON.stringify(result)}`);
+    // 비즈니스 로직 호출
+    const result = await stationService.reg(params);
+    logger.info(`(station.reg.result) ${JSON.stringify(result)}`);
 
-//     // 최종 응답
-//     res.status(200).json(result);
-//   } catch (err) {
-//     res.status(500).json({ err: err.toString() });
-//   }
-// });
+    // 최종 응답
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ err: err.toString() });
+  }
+});
 
 // 리스트 조회
 router.get('/', isLoggedIn, async (req, res) => {
@@ -75,7 +75,7 @@ router.delete('/', isLoggedIn, async (req, res) => {
   }
 });
 
-router.post('/', isLoggedIn, async (req, res) => {
+router.post('/search', isLoggedIn, async (req, res) => {
   try {
     const params = {
       // START_INDEX: req.body.START_INDEX,
